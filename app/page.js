3e0b131e903b9691049a259ccbc62b5bd2b2356f
mp20 <ariyanazari1380@gmail.com>
2024-01-03 18:47:22 -0500

    added to dashboard

--- app/page.js
+++ app/page.js
@@ -1,10 +1,31 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import "./main.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Dash</h1>
+    <main>
+      <div className="main-grid">
+        <div className="left-column">
+          <div className="left-column-flex">
+            <p>
+              Hi! I'm Ariya, <br /> A Software
+              <br /> Engineer
+            </p>
+            <div className="project-experience-buttons">
+              <button>Experience</button>
+              <button>Projects</button>
+            </div>
+          </div>
+        </div>
+        <div className="right-column">
+          <Image
+            src="/mywebsite.png"
+            alt="Picture of the author"
+            width={724}
+            height={724}
+          />
+        </div>
+      </div>
     </main>
   );
 }
